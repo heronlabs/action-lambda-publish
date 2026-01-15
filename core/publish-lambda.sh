c2566979bd4f3334a72364234f5af6a7116e2674
Lucas Lacerda <lucaslacerdacl@gmail.com>
2026-01-15 11:35:36 -0300

    feat: add GitHub Action for publishing AWS Lambda functions with CI workflow

--- core/publish-lambda.sh
+++ core/publish-lambda.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+set -euo pipefail
 
 aws lambda update-function-code \
---function-name ${LAMBDA_NAME} \
---image-uri ${LAMBDA_IMAGE}
+--function-name "${LAMBDA_NAME}" \
+--image-uri "${LAMBDA_IMAGE}"
